4a7c7708312e15b19e3e41272bf6e848590e34f3
Patrick McLean <patrickmclean@yahoo.com>
2021-05-08 19:01:41 +0200

    update server

--- app/app.js
+++ app/app.js
@@ -205,7 +205,7 @@ app.get('/serverstream', (req, res) => {
  });
 
 // The server itself
-var server = app.listen(8081, function () {
+var server = app.listen(8082, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("App listening at http://%s:%s", host, port)
---
+++ app/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  logfile_directory: './logs',
+  aws_table_name: 'mcChat',
+  aws_users_table: 'mcUsers',
+  aws_token_table: 'mcTokens',
+  aws_remote_config: {
+    accessKeyId: 'AKIAXEXXAIIBKIL6WHPH',
+    secretAccessKey: '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',
+    region: 'us-east-2',
+  },
+  log_level: 2,
+  log_to_file: true,
+  log_to_console: true,
+};
